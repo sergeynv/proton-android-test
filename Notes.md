337ec399fa1c865a1844f204a23f9ebaa82ec4cd
Sergey Nikolaienkov <sergeynv@google.com>
2022-02-11 16:44:57 +0100

    Give MainActivity bottom navigation

--- Notes.md
+++ Notes.md
@@ -3,4 +3,5 @@
 ## Bottom Navigation
 
 - [BottomNavigationView](https://developer.android.com/reference/com/google/android/material/bottomnavigation/BottomNavigationView)
-- [Bottom navigation](https://developer.android.com/guide/navigation/navigation-ui#bottom_navigation)
+- [developer.android.com/guide](https://developer.android.com/guide/navigation/navigation-ui#bottom_navigation)
+- [material.io]https://material.io/components/bottom-navigation/android#using-bottom-navigation
--- app/src/main/java/ch/protonmail/android/protonmailtest/MainActivity.kt
+++ app/src/main/java/ch/protonmail/android/protonmailtest/MainActivity.kt
@@ -1,23 +1,38 @@
 package ch.protonmail.android.protonmailtest
 
-import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.viewpager.widget.ViewPager
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
-
-    @SuppressLint("WrongConstant")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val pager = findViewById<ViewPager>(R.id.pager)
-        val adapter = TabsAdapter(this, supportFragmentManager)
-        pager.adapter = adapter
+//        val pager = findViewById<ViewPager>(R.id.pager)
+//        val adapter = TabsAdapter(this, supportFragmentManager)
+//        pager.adapter = adapter
+
+        findViewById<BottomNavigationView>(R.id.bottom_navigation).setOnItemSelectedListener {
+            onNavigationItemSelected(it.itemId)
+        }
     }
 
-    fun initTabs() {
+    private fun onNavigationItemSelected(id: Int): Boolean {
+        when (id) {
+            R.id.navigation_item_upcoming -> Log.d(TAG, """Selected "Upcoming"""")
+            R.id.navigation_item_hottest -> Log.d(TAG, """Selected "Hottest"""")
+            else -> return false
+        }
+        return true
+    }
+
+    private fun initTabs() {
         //TODO
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
     }
 }
