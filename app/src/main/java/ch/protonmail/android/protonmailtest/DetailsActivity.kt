03b7f0de667421488f5f53a4510e86a502e8704b
Sergey Nikolaienkov <sergeynv@google.com>
2022-02-14 19:30:28 +0100

    Dowload image by a click on the 'Download' button

--- app/src/main/java/ch/protonmail/android/protonmailtest/DetailsActivity.kt
+++ app/src/main/java/ch/protonmail/android/protonmailtest/DetailsActivity.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -13,11 +14,19 @@ import com.squareup.picasso.Picasso
  * Shows all the details for a particular day.
  */
 class DetailsActivity : AppCompatActivity() {
+    private lateinit var imageView: ImageView
+    private lateinit var imageLabel: TextView
+    private lateinit var btnDownload: View
+
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_details)
+
+        imageView = findViewById(R.id.image_view)
+        imageLabel = findViewById(R.id.label_image)
+        btnDownload = findViewById(R.id.btn_download)
 
         with(intent.getParcelableExtra<DayForecast>(EXTRA_DAY_FORECAST)!!) {
             title = getString(R.string.title_day_index, dayIndex)
@@ -27,12 +36,11 @@ class DetailsActivity : AppCompatActivity() {
             findViewById<TextView>(R.id.temp).text = "$low / $high"
             findViewById<TextView>(R.id.sun).text = "$sunrise / $sunset"
 
-            val imageView = findViewById<ImageView>(R.id.image_view)
-            loadImage(imageUrl, imageView)
+            btnDownload.setOnClickListener { loadImage(imageUrl) }
         }
     }
 
-    private fun loadImage(url: String, view: ImageView) = Picasso.get().load(url).into(view)
+    private fun loadImage(url: String) = Picasso.get().load(url).into(imageView)
 
     companion object {
         private const val EXTRA_DAY_FORECAST =
