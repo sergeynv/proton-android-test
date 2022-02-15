e6357f479c0f679a53990191a43b8322ced01017
Sergey Nikolaienkov <sergeynv@google.com>
2022-02-15 08:39:48 +0100

    Tweak PicassoUtils

--- app/src/main/java/ch/protonmail/android/protonmailtest/DetailsActivity.kt
+++ app/src/main/java/ch/protonmail/android/protonmailtest/DetailsActivity.kt
@@ -6,7 +6,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.view.View.GONE
-import android.view.View.INVISIBLE
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -37,25 +36,40 @@ class DetailsActivity : AppCompatActivity() {
             findViewById<TextView>(R.id.temp).text = "$low / $high"
             findViewById<TextView>(R.id.sun).text = "$sunrise / $sunset"
 
-            btnDownload.setOnClickListener {
-                imageLabel.setText(R.string.label_image_downloading)
-                loadImageInto(imageView) { success ->
-                    if (success) {
-                        onImageLoaded()
-                    } else {
-                        imageLabel.setText(R.string.label_image_download_failed)
-                    }
-                }
-            }
+            btnDownload.setOnClickListener { loadImage(dayForecast = this) }
 
-            // Also try loading form cache
-            loadImageFromCacheInto(imageView) { onImageLoaded() }
+            // Also try loading form cache right now.
+            loadImage(dayForecast = this, fromCacheOnly = true)
         }
     }
 
-    private fun onImageLoaded() {
-        btnDownload.visibility = INVISIBLE
-        imageLabel.visibility = GONE
+    override fun onDestroy() {
+        // Need to cancel the image load request, so that we do not leak the ImageView, the
+        // Callback and the whole Activity along with them.
+        // (Actually would be great to move this to a ViewModel, and keep the request going if this
+        // is simply a configuration change, but that feels out of scope for a test assignment.)
+        cancelLoadImage(imageView)
+
+        super.onDestroy()
+    }
+
+    private fun loadImage(dayForecast: DayForecast, fromCacheOnly: Boolean = false) {
+        // Only show "Downloading..." label if this is a network request.
+        if (!fromCacheOnly) imageLabel.setText(R.string.label_image_downloading)
+        // Disable the download button while loading.
+        btnDownload.isEnabled = false
+
+        dayForecast.loadImageInto(fromCacheOnly, imageView) { success: Boolean ->
+            if (success) {
+                btnDownload.visibility = GONE
+                imageLabel.visibility = GONE
+            } else {
+                // Only show "Failed" label if this was a network request.
+                if (!fromCacheOnly) imageLabel.setText(R.string.label_image_download_failed)
+
+                btnDownload.isEnabled = true
+            }
+        }
     }
 
     companion object {
--- app/src/main/java/ch/protonmail/android/protonmailtest/PicassoUtils.kt
+++ app/src/main/java/ch/protonmail/android/protonmailtest/PicassoUtils.kt
@@ -4,20 +4,31 @@ import android.widget.ImageView
 import com.squareup.picasso.Callback
 import com.squareup.picasso.NetworkPolicy.OFFLINE
 import com.squareup.picasso.Picasso
+import com.squareup.picasso.RequestCreator
 
+fun DayForecast.loadImageInto(
+    fromCacheOnly: Boolean,
+    imageView: ImageView,
+    callbackAction: ((Boolean) -> Unit)? = null
+) = requestCreator.apply { if (fromCacheOnly) networkPolicy(OFFLINE) }
+    .into(imageView, callbackAction)
+
+fun cancelLoadImage(imageView: ImageView) = Picasso.get().cancelRequest(imageView)
+
+// All the DayForecast objects returned by REST service (mockapi.io) seem to have image (precisely:
+// https://picsum.photos/640/480). While the picsum.photos service responds with a different picture
+// to every response, the regular caching - using the URL as the cache key - won't work here: we'll
+// load the image for on  the days once, it will get cached and will get re-used for all the other
+// days as well. To work around this we'll manually specify a different Picasso's stableKey (
+// basically a cache key) for every day, which will be a combination of the url and the day index.
+// E.g. "https://picsum.photos/640/480:day-2".
 private val DayForecast.requestCreator
-    get() = Picasso.get().load(imageUrl).stableKey("$imageUrl/day-$dayIndex")
+    get() = Picasso.get().load(imageUrl).stableKey("$imageUrl:day-$dayIndex")
 
-fun DayForecast.loadImageInto(imageView: ImageView, callbackAction: (Boolean) -> Unit) {
-    requestCreator.into(imageView, object : Callback {
+// Making the Picasso API more Kotlin-friendly (this into() method takes callback in form of a
+// lambda, so the callers would be able use "trailing lambda" syntax when invoking the API).
+private fun RequestCreator.into(imageView: ImageView, callbackAction: ((Boolean) -> Unit)?) =
+    into(imageView, if (callbackAction == null) null else object : Callback {
         override fun onSuccess() = callbackAction(true)
         override fun onError(e: Exception?) = callbackAction(false)
     })
-}
-
-fun DayForecast.loadImageFromCacheInto(imageView: ImageView, callbackAction: () -> Unit) {
-    requestCreator.networkPolicy(OFFLINE).into(imageView, object : Callback {
-        override fun onSuccess() = callbackAction()
-        override fun onError(e: Exception?) { }
-    })
-}
